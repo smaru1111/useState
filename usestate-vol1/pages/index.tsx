2b920599db09f5ae53407a7a979f7e6b1ceeaa9a
smaru1111 <96244711+smaru1111@users.noreply.github.com>
2023-04-02 21:09:54 +0900

    index.tsxのフォーマット

--- usestate-vol1/pages/index.tsx
+++ usestate-vol1/pages/index.tsx
@@ -1,52 +1,10 @@
 import { useState } from "react";
 
 const App = () => {
-  // 作成したtodoを入れておくためのstate
-  const [todos, setTodos] = useState<string[]>([]);
-  // フォームに入力された値をtodoに登録するまでに入れておくためのstate
-  const [tmpTodo, setTmpTodo] = useState("");
-
-  const addTodo = () => {
-    setTodos([...todos, tmpTodo]);
-    setTmpTodo("");
-  };
-  const delTodo = () => {
-    setTodos(
-      todos.filter((todo, index) => (index !== index))
-  )
-  };
 
   return (
     <>
-      <h1>Todo App</h1>
-      <div className="form">
-        <input
-          type="text"
-          name="todo"
-          // formの入力値をtmpTodoで持っておく
-          onChange={e => setTmpTodo(e.target.value)}
-          value={tmpTodo}
-        />
-        <button onClick={addTodo}>Add</button>
-      </div>
-      <ul>
-        {todos.map((todo, index) => {
-          return <li key={index} onClick={delTodo}>{todo}</li>;
-        })}
-      </ul>
-      <style>{`
-        h1 {
-          text-align: center;
-        }
-        .form {
-          display: flex;
-          justify-content: center;
-        }
-        ul {
-          width: 200px;
-          margin: 10px auto;
-        }
-      `}</style>
+      
     </>
   );
 };
